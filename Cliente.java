373ed20da5d53171884d1d382ae0647dbec65706
anajulia0911 <anajblande04@gmail.com>
2024-11-18 20:35:12 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,19 @@
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (CPF: " + cpf + ")";
+    }
+}
