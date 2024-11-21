f74a05c515bc5bac1b52d28324af22a48981e341
anajulia0911 <anajblande04@gmail.com>
2024-11-21 19:25:02 -0300

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,10 +1,41 @@
-
 public abstract class Entrada {
     protected int numeroDoAssento;
+    protected double preco;
 
-    public Entrada(int numeroDoAssento) {
+    public Entrada(int numeroDoAssento, double preco) {
         this.numeroDoAssento = numeroDoAssento;
+        this.preco = preco;
     }
 
     public abstract double calculaValor();
+}
+
+class EntradaInteira extends Entrada {
+    public EntradaInteira(int numeroDoAssento, double preco) {
+        super(numeroDoAssento, preco);
+    }
+
+    public double calculaValor() {
+        return preco;
+    }
+}
+
+class EntradaMeia extends Entrada {
+    public EntradaMeia(int numeroDoAssento, double preco) {
+        super(numeroDoAssento, preco);
+    }
+
+    public double calculaValor() {
+        return preco / 2;
+    }
+}
+
+class EntradaProfessor extends Entrada {
+    public EntradaProfessor(int numeroDoAssento, double preco) {
+        super(numeroDoAssento, preco);
+    }
+
+    public double calculaValor() {
+        return preco * 0.4;
+    }
 }
